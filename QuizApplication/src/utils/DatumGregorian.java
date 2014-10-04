dd0c3b30b0cb1de2617a90898ea82cf865179727
Bart Blondeel <Bart_Blondeel@hotmail.be>
2014-10-04 12:26:53 +0200

    Change private prop

--- QuizApplication/src/utils/DatumGregorian.java
+++ QuizApplication/src/utils/DatumGregorian.java
@@ -6,31 +6,31 @@ import java.util.GregorianCalendar;
 public class DatumGregorian {
 
 	public DatumGregorian() {
-		gregorianDatum = (GregorianCalendar) GregorianCalendar.getInstance();
+		gregDatum = (GregorianCalendar) GregorianCalendar.getInstance();
 	}
 
 	public DatumGregorian(int dag, int maand, int jaar) {
-		gregorianDatum = new GregorianCalendar(jaar, maand, dag);
+		gregDatum = new GregorianCalendar(jaar, maand, dag);
 	}
 
 	public DatumGregorian(String inputDatum) {
 		String[] temp = inputDatum.split("/");
-		gregorianDatum = new GregorianCalendar(Integer.parseInt(temp[2]), Integer.parseInt(temp[1]), Integer.parseInt(temp[0]));
+		gregDatum = new GregorianCalendar(Integer.parseInt(temp[2]), Integer.parseInt(temp[1]), Integer.parseInt(temp[0]));
 	}
 
-	private GregorianCalendar gregorianDatum;
+	private GregorianCalendar gregDatum;
 
 	public String getDatumInAmerikaansFormaat() {
-		return new SimpleDateFormat("yyyy/mm/dd").format(gregorianDatum);
+		return new SimpleDateFormat("yyyy/mm/dd").format(gregDatum);
 	}
 
 	public String getDatumInEuropeesFormaat() {
-		return new SimpleDateFormat("dd/mm/yyyy").format(gregorianDatum);
+		return new SimpleDateFormat("dd/mm/yyyy").format(gregDatum);
 	}
 
 	public boolean setDatum(int jaar, int maand, int dag) {
 		try {
-			gregorianDatum = new GregorianCalendar(jaar, maand, dag);
+			gregDatum = new GregorianCalendar(jaar, maand, dag);
 		} catch (IllegalArgumentException ex) {
 			return false;
 		}
