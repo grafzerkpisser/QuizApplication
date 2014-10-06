848549abf82ca0a925c76a90828b8660df3c50d9
eli <eli@eli-TOSH>
2014-10-06 21:02:34 +0200

    enums

---
+++ Quiz/src/utils/Enums.java
@@ -0,0 +1,7 @@
+package utils;
+
+public class Enums {
+	public enum Dagvanweek { maandag, dinsdag, woensdag, donderdag, vrijdag, zaterdag, zondag};
+	public enum Maand {januari, februari, maart, april, mei, juni, juli, augustus, september, oktober, november, december  }
+	
+}
