7c5d9245283a06d91022389149920725a4ce653b
Gavin <gavin.bogaerts@telenet.be>
2014-12-01 16:20:31 +0100

    Added states

---
+++ QuizApplication/src/QuizStates/QuizInConstructionState.java
@@ -0,0 +1,58 @@
+package QuizStates;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+import model.opdracht.QuizOpdracht;
+import model.quiz.Quiz;
+
+public class QuizInConstructionState implements QuizState {
+
+	@Override
+	public void verwijderQuizOpdracht(Quiz quiz, QuizOpdracht quizOpdracht) {
+		ArrayList<QuizOpdracht> teVerwijderenQuizOpdrachten = new ArrayList<QuizOpdracht>();
+		for (QuizOpdracht qO : quiz.getEenQuizOpdrachtLijst()) {
+			if (qO.equals(quizOpdracht)) {
+
+				teVerwijderenQuizOpdrachten.add(quizOpdracht);
+			}
+		}
+
+		for (QuizOpdracht qO : teVerwijderenQuizOpdrachten) {
+			quiz.getEenQuizOpdrachtLijst().remove(qO);
+		}		
+	}
+
+	@Override
+	public void voegQuizOpdrachtToe(Quiz quiz, QuizOpdracht quizOpdracht) {
+		ArrayList<QuizOpdracht> toeTeVoegenQuizOpdrachten = new ArrayList<QuizOpdracht>();
+		for (QuizOpdracht qO : quiz.getEenQuizOpdrachtLijst()) {
+			if (qO.equals(quizOpdracht)) {
+
+				toeTeVoegenQuizOpdrachten.add(qO);
+			}
+		}
+
+		if (toeTeVoegenQuizOpdrachten.size() != 0) {
+			for (QuizOpdracht qO : toeTeVoegenQuizOpdrachten) {
+				quiz.getEenQuizOpdrachtLijst().add(qO);
+			}
+		}
+	}
+
+	@Override
+	public void verwijderQuiz(Quiz quiz) {
+		for(QuizOpdracht quizOpdracht : quiz.getEenQuizOpdrachtLijst())
+		{
+			quizOpdracht.ontkoppelOpdrachtVanQuiz();
+		}
+		System.out.printf("Quiz %s is nog niet gekoppeld aan de QuizCatalogus.", quiz);
+	}
+
+	@Override
+	public void neemDeelAanQuiz(Quiz quiz) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
