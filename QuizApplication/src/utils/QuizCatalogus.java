bc403ea192be939131c6526f2a1e66e427cb37a1
eli <eli@eli-TOSH>
2014-12-01 19:47:02 +0100

    hyg

--- QuizApplication/src/utils/QuizCatalogus.java
+++ QuizApplication/src/utils/QuizCatalogus.java
@@ -1,65 +1,56 @@
 package utils;
 
-import model.opdracht.Opdracht;
-import model.opdracht.OpdrachtCatalogus;
-
+import java.io.Serializable;
 import java.util.ArrayList;
-//import java.util.Collections;
-//import java.util.Iterator;
-//import java.io.File;
-//import java.util.Scanner;
-
+import java.util.Iterator;
 
 import model.quiz.Quiz;
-public abstract class QuizCatalogus implements Iterable <Quiz>{
-	
-	// is hiermee de associatie met Quiz af
-	public ArrayList <Quiz> quiznaam =new ArrayList <Quiz>();
-	
-	// nog exceptions opvangen
-	
-	public void addQuizToCatalogue (Quiz q){
-		//exception: is q wel een quiz? ArrayStoreException
-		try {
-		quiznaam.add(q);
-		}
-		catch(ArrayStoreException f){
-			System.out.println("Fout formaat");
-		}
-		
-		finally{
-			System.out.println("Quiz niet toegevoegd");
-		}
-		
-	}
-	
-	public void deleteQuizFromCatalogue( Quiz q){
-		// enkel te verwijderen indien status in constructie of afgewerkt
-		q.getQuizStatus();
-		
-		quiznaam.remove(q);
-	}
-		
-	public String toString(){
-		String result = "";
-		for ( Quiz q : quiznaam) //for each
-			result += "Quiz: " + q + "\n";
-		return result;
-	}
-	
-	public int compareTo (QuizCatalogus QC){
-		return QC.compareTo(QC);
-	}
 
+@SuppressWarnings("serial")
+public class QuizCatalogus implements Iterable<QuizCatalogus>,
+Comparable<QuizCatalogus>, Cloneable, Serializable {
+	
+	public ArrayList<Quiz> quizLijst;
+	public QuizCatalogus() {
+		quizLijst = new ArrayList<Quiz>();
+	}
+	public void voegQuizToe(Quiz q) {
+		quizLijst.add(q);
+	}
+	
+	public void verwijderQuiz(Quiz q) throws Exception {
+		if (q.getEenQuizOpdrachtLijst().size() > 0)
+			throw new Exception(
+					"Quiz kan niet verwijderd worden wanneer er reeds opdrachten zijn aan toegevoegd");
+		else
+			quizLijst.remove(q);
+	}
+	
+	@Override
+	public QuizCatalogus clone() throws CloneNotSupportedException {
+
+		QuizCatalogus q_c = new QuizCatalogus();
+		q_c.quizLijst = this.quizLijst;
+		return q_c;
+	}
+	
+	@Override
+	public String toString() {
+		String quizen = "";
+		for (Quiz q : quizLijst) {
+			quizen += q.toString() + "/n";
+		}
+		return quizen;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result
-				+ ((quiznaam == null) ? 0 : quiznaam.hashCode());
+				+ ((quizLijst == null) ? 0 : quizLijst.hashCode());
 		return result;
 	}
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -69,11 +60,22 @@ public abstract class QuizCatalogus implements Iterable <Quiz>{
 		if (getClass() != obj.getClass())
 			return false;
 		QuizCatalogus other = (QuizCatalogus) obj;
-		if (quiznaam == null) {
-			if (other.quiznaam != null)
+		if (quizLijst == null) {
+			if (other.quizLijst != null)
 				return false;
-		} else if (!quiznaam.equals(other.quiznaam))
+		} else if (!quizLijst.equals(other.quizLijst))
 			return false;
 		return true;
 	}
+	@Override
+	public int compareTo(QuizCatalogus q) {
+		return q.compareTo(this);
+	}
+
+	@Override
+	public Iterator<QuizCatalogus> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
